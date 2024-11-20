62e6d7cf5f9d4632350766c5267eadb555fc9f8b
秋风 <xzn259750@163.com>
2024-11-20 13:56:50 +0800

    adding my files to my repository

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+const list = document.createElement('ul');
+const info = document.createElement('p');
+const html = document.querySelector('html');
+
+info.textContent = '输入的数据.';
+
+document.body.appendChild(info);
+document.body.appendChild(list);
+
+html.onclick = function() {
+  const listItem = document.createElement('li');
+  const listContent = prompt('What content do you want the list item to have?');
+  listItem.textContent = listContent;
+  list.appendChild(listItem);
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    const listContent = prompt('Enter new content for your list item');
+    this.textContent = listContent;
+  }
+}
